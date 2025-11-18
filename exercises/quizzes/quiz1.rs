bc48fca6a91df59bcdf25b5d8cee8c8335b8f036
mahelika <imahelika@gmail.com>
2025-11-18 21:15:53 +0530

    quiz1.rs

--- exercises/quizzes/quiz1.rs
+++ exercises/quizzes/quiz1.rs
@@ -12,6 +12,14 @@
 // the quantity bought.
 // fn calculate_price_of_apples(???) -> ??? { ??? }
 
+fn calculate_price_of_apples(no_of_apples: u64)-> u64 {
+    if no_of_apples > 40 {
+        no_of_apples
+    } else {
+        no_of_apples * 2
+    }
+}
+
 fn main() {
     // You can optionally experiment here.
 }
